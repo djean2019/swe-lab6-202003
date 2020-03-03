4cfc89a498a4fb95d7482563f53406e2fd1aae7b
Darhyppo <darhyppo@gmail.com>
2020-03-03 17:49:11 -0600

    SecondBiggest Updated

--- src/codingpractice/SecondBiggest.java
+++ src/codingpractice/SecondBiggest.java
@@ -1,15 +1,23 @@
 package codingpractice;
 
+import java.util.Arrays;
+
 public class SecondBiggest {
 	public static void main(String[] args) {
 		int[] a= {1,2,3,4,5};
 		int[] b= {19,9,11,0,12};
-		System.out.println("The second Bigger integer in {1,2,3,4,5} is \n"+findSecondBiggest(a));
-		System.out.println("The second Bigger integer in {19,9,11,0,12} is \n"+findSecondBiggest(b));
+		if(findSecondBiggest(a)==Integer.MIN_VALUE)
+			System.out.println("There's no second biggest value in the array '"+Arrays.toString(a)+"'");
+		else
+			System.out.println("The second Bigger integer in "+Arrays.toString(a)+" is \n"+findSecondBiggest(a));
+		if(findSecondBiggest(b)==Integer.MIN_VALUE)
+			System.out.println("There's no second biggest value in the array '"+Arrays.toString(b)+"'");
+		else
+			System.out.println("The second Bigger integer in "+Arrays.toString(b)+" is \n"+findSecondBiggest(b));
 	}
 	
 	public static int findSecondBiggest(int[] arr) {
-		if(arr==null || arr.length==0) return 0;
+		if(arr==null || arr.length<2) return Integer.MIN_VALUE;
 		int max=arr[0];
 		int secondBig=Integer.MIN_VALUE;
 		for(int i:arr) {
