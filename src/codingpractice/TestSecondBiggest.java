beb6dbde8d88aa1d2c9e3f3db0ecd7a3b09e2c41
Darhyppo <darhyppo@gmail.com>
2020-03-03 18:00:19 -0600

    packages Updated

--- src/codingpractice/TestSecondBiggest.java
+++ src/codingpractice/TestSecondBiggest.java
@@ -9,9 +9,11 @@ class TestSecondBiggest {
 	@Test
 	void test() {
 		int[] a= {1,2,3,4,5};
-		int[] b= {19,9,11,0,12};	
+		int[] b= {19,9,11,0,12};
+		int[] c= {12};
 		assertEquals(12,SecondBiggest.findSecondBiggest(b));
 		assertEquals(4,SecondBiggest.findSecondBiggest(a));
+		assertEquals(Integer.MIN_VALUE,SecondBiggest.findSecondBiggest(c));
 	}
 
 }
